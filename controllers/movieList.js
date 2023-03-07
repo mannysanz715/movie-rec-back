2169fd7a825a5d3f7cff238bfdecf29a5a231c7a
mannysanz715 <mannysanz715@gmail.com>
2023-03-07 15:27:38 -0800

    Add Random Movie

--- controllers/movieList.js
+++ controllers/movieList.js
@@ -6,12 +6,14 @@ async function getMovieList(req, res){
   try {
     const genreKey = req.params.id
     const apiURLList = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_original_language=en&with_genres=${genreKey}&vote_average.gte=7`
+
     let listData = await fetch(apiURLList)
     listData = await listData.json(listData)
+
     const movie = selectRandomMovie(listData)
+
     return await res.status(200).json(movie)
   } catch (error) {
-    console.log(error)
     return res.status(500).json(error)
   }
 }
